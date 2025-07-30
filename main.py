86ba60bc25b57a58f839e739ebae16388ce2c326
sd2879 <suman8deb@gmail.com>
2025-07-30 18:17:39 +0100

    updating agent

--- main.py
+++ main.py
@@ -45,22 +45,20 @@ async def create_agent(coral_tools, agent_tools):
         model_provider=os.getenv("MODEL_PROVIDER", "openai"),
         api_key=os.getenv("MODEL_API_KEY"),
         temperature=os.getenv("MODEL_TEMPERATURE", "0.1"),
-        max_tokens=os.getenv("MODEL_TOKEN_LIMIT", "8000")
+        max_tokens=os.getenv("MODEL_MAX_TOKENS", "8000"),
+        base_url=os.getenv("MODEL_BASE_URL", None)
     )
     agent = create_tool_calling_agent(model, combined_tools, prompt)
     return AgentExecutor(agent=agent, tools=combined_tools, verbose=True, handle_parsing_errors=True)
 
 async def main():
 
-    runtime = os.getenv("CORAL_ORCHESTRATION_RUNTIME", "devmode")
-
-    if runtime == "docker" or runtime == "executable":
-        base_url = os.getenv("CORAL_SSE_URL")
-        agentID = os.getenv("CORAL_AGENT_ID")
-    else:
+    runtime = os.getenv("CORAL_ORCHESTRATION_RUNTIME", None)
+    if runtime is None:
         load_dotenv()
-        base_url = os.getenv("CORAL_SSE_URL")
-        agentID = os.getenv("CORAL_AGENT_ID")
+
+    base_url = os.getenv("CORAL_SSE_URL")
+    agentID = os.getenv("CORAL_AGENT_ID")
 
     coral_params = {
         "agentId": agentID,
